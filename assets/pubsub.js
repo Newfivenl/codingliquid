0e4068449fbb1aea0361362b0f9de2faf721ce35
Newfive <info@newfive.nl>
2024-10-09 16:53:33 +0200

    Push arch type theme

--- assets/pubsub.js
+++ assets/pubsub.js
@@ -1,23 +1,21 @@
-let subscribers = {};
+export function subscribe(eventName, callback) {
+  let cb = (event) => callback(event)
 
-function subscribe(eventName, callback) {
-  if (subscribers[eventName] === undefined) {
-    subscribers[eventName] = [];
-  }
-
-  subscribers[eventName] = [...subscribers[eventName], callback];
+  document.addEventListener(eventName, cb)
 
   return function unsubscribe() {
-    subscribers[eventName] = subscribers[eventName].filter((cb) => {
-      return cb !== callback;
-    });
-  };
+    document.removeEventListener(eventName, cb)
+  }
 }
 
-function publish(eventName, data) {
-  if (subscribers[eventName]) {
-    subscribers[eventName].forEach((callback) => {
-      callback(data);
-    });
-  }
+export function publish(eventName, options) {
+  document.dispatchEvent(new CustomEvent(eventName, options))
+}
+
+export const EVENTS = {
+  cartBeforeChange: 'cart:before-change',
+  cartChange: 'cart:change',
+  cartError: 'cart:error',
+  lineItemChange: 'line-item:change',
+  variantChange: 'variant:change'
 }
