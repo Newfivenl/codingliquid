406079d777ab7def63be0ec77c79091b6daec767
NF - Roni <roni@newfive.dev>
2024-10-18 17:21:56 +0800

    shop the look

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    mode: 'jit',
     content: [
       './assets/*.liquid',
       './config/*.json',
@@ -12,6 +13,20 @@ module.exports = {
     ],
     theme: {
       extend: {
+        animation: {
+          marquee: 'marquee 25s linear infinite',
+          marquee2: 'marquee2 25s linear infinite'
+        },
+        keyframes: {
+          marquee: {
+            '0%': { transform: 'translateX(-100%)' },
+            '100%': { transform: 'translateX(0%)' },
+          },
+          marquee2: {
+            '0%': { transform: 'translateX(0%)' },
+            '100%': { transform: 'translateX(100%)' },
+          },
+        },
       },
     },
     plugins: [
